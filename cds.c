7d079c42714775728efd3d347646963fbf603103
wasja <wasja_duecker@gmx.de>
2011-05-01 00:01:39 -0700

    Edited cds.c via GitHub

--- cds.c
+++ cds.c
@@ -14,17 +14,18 @@ void createCD()
     system("CLS");
     printf("Erfassung einer neuen CD\n");
     printLine('-', 30);
-    getText("Geben Sie bitte den Titel der CD ein:\n", 30, 0, &((CDData + CountCDs) -> Titel));
-    getText("Geben Sie bitte den Interpreten ein:\n(dieses Feld leer lassen, wenn es sich um unterschiedliche Interpreten handelt)\n",30,1, &((CDData + CountCDs) -> Interpret));
-    getNumber("Geben Sie bitte das Erscheinungsjahr ein:", &((CDData + CountCDs) -> Erscheinungsjahr), 1, 2020);
-    getNumber("Geben Sie bitte die Anzahl der Lieder ein:", &((CDData + CountCDs) -> AnzahlSongs), 1, 50);
+    getText("Geben Sie bitte den Titel der CD ein:\n->", MAXSONGS, 0, &((CDData + CountCDs) -> Titel));
+    getText("Geben Sie bitte den Interpreten ein:\n(dieses Feld leer lassen, wenn es sich um unterschiedliche Interpreten handelt)\n->",MAXSONGS, 1, &((CDData + CountCDs) -> Interpret));
+    getNumber("Geben Sie bitte das Erscheinungsjahr ein:\n->", &((CDData + CountCDs) -> erscheinungsjahr), 1, 2020);
+    getNumber("Geben Sie bitte die Anzahl der Lieder ein:\n->", &((CDData + CountCDs) -> AnzahlSongs), 1, MAXSONGS);
 
     do
     {
-        getText("Bitte den Titel des Liedes eingeben:", 30, 0, &(((CDData + CountCDs) -> Lied) -> Titel));
-        //if(!(CDData -> Interpret)) funzt noch nicht
-        getText("Bitte den Interpreten des Liedes eingeben:", 30, 0, &(((CDData + CountCDs) -> Lied) -> Interpret));
 
+        printf("Lied %i:\n", (counter + 1));
+        getText("Bitte den Titel des Liedes eingeben:\n->", MAXSONGS, 0, &(((CDData + CountCDs) -> Lied[counter]).Titel));
+        //if(!(CDData -> Interpret)) funzt noch nicht
+        getText("Bitte den Interpreten des Liedes eingeben:\n->", MAXSONGS, 0, &(((CDData + CountCDs) -> Lied[counter]).Interpret));
         counter++;
 
     }while(counter < ((CDData + CountCDs) -> AnzahlSongs));
@@ -70,5 +71,40 @@ void sortCDs()
 
 void listCDs()
 {
-    printf("listCDs\n");
+    int counter =0;
+
+    system("CLS");
+    printf("Liste der CDs\n");
+    printLine('-', 30);
+
+    do
+    {
+        listOneCD();
+    }while(counter < CountCDs);
+
+    clearBuffer();
+
 }
+void listOneCD()
+{
+    int counter = 0;
+
+    printf("Titel           : %s\n", ((CDData + CountCDs) -> Titel));
+    printf("Erscheinungsjahr: %i\n", ((CDData + CountCDs) -> erscheinungsjahr));
+    printf("Anzahl Lieder   : %i\n", ((CDData + CountCDs) -> AnzahlSongs));
+
+    do
+    {
+        listOneSong(counter);
+        counter++;
+    }while(counter < ((CDData + CountCDs) -> AnzahlSongs));
+}
+
+void listOneSong(int counter)
+{
+
+
+        printf("    0%i. %s ", (counter + 1), ((CDData + CountCDs) -> Lied[counter].Titel));
+        printf("(%s;)\n", ((CDData + CountCDs) -> Lied[counter].Interpret));
+}
+
